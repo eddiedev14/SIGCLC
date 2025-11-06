d697d2c58eab479ca613a019eff150fa331af54e
Christian Home <davidhome365@gmail.com>
2025-11-05 21:44:57 -0500

    ✨ feat: add ForoMapper

---
+++ src/main/java/com/backend/sigclc/Mapper/ForoMapper.java
@@ -0,0 +1,68 @@
+package com.backend.sigclc.Mapper;
+
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Component;
+
+import com.backend.sigclc.DTO.Foros.ForoCreateDTO;
+import com.backend.sigclc.DTO.Foros.ForoResponseDTO;
+import com.backend.sigclc.DTO.Foros.ForoUpdateDTO;
+import com.backend.sigclc.DTO.Foros.Moderador.ModeradorResponseDTO;
+import com.backend.sigclc.Model.Foros.ForosModel;
+import com.backend.sigclc.Model.Foros.ModeradorModel;
+
+@Component
+public class ForoMapper {
+    
+    public ForosModel toModel(ForoCreateDTO dto) {
+        ForosModel model = new ForosModel();
+        model.setTipoTematica(dto.getTipoTematica());
+        model.setNombreTematica(dto.getNombreTematica());
+        model.setModerador(toModeradorModel(dto.getModeradorId()));
+        return model;
+    }
+
+    public ModeradorModel toModeradorModel(ObjectId moderadorId) {
+        ModeradorModel model = new ModeradorModel();
+        model.setModeradorId(moderadorId);
+        return model;
+    }
+
+
+    public ForoResponseDTO toResponseDTO(ForosModel model) {
+        ForoResponseDTO dto = new ForoResponseDTO();
+        dto.setId(model.getId());
+        dto.setTipoTematica(model.getTipoTematica());
+        dto.setNombreTematica(model.getNombreTematica());
+        dto.setFechaPublicacion(model.getFechaPublicacion());
+        dto.setModerador(toModeradorDTOResponse(model.getModerador()));
+        return dto;
+    }
+
+    public ModeradorResponseDTO toModeradorDTOResponse(ModeradorModel model) {
+        ModeradorResponseDTO dto = new ModeradorResponseDTO();
+        dto.setModeradorId(model.getModeradorIdAsString());
+        dto.setNombreCompleto(model.getNombreCompleto());
+        return dto;
+    }
+
+    /**
+     * Convierte una lista de UsuariosModel a lista de UsuarioResponseDTO
+     */
+    public List<ForoResponseDTO> toResponseDTOList(List<ForosModel> models) {
+        return models.stream()
+                .map(this::toResponseDTO)
+                .toList();
+    }
+
+    /**
+     * Actualiza un UsuariosModel existente con los datos de UsuarioUpdateDTO
+     */
+    public void updateModelFromDTO(ForoUpdateDTO dto, ForosModel model) {
+        if (dto.getTipoTematica() != null) model.setTipoTematica(dto.getTipoTematica());
+        if (dto.getNombreTematica() != null) model.setNombreTematica(dto.getNombreTematica());
+    }
+
+}
