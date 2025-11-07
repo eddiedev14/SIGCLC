96fe065a8a4ff4ba843984d1a73cff516b32ee03
Eddie Santiago Delgado <eddiesantiago122007@gmail.com>
2025-11-07 07:34:44 -0500

    Revert "✨ feat: add two actions on UsuariosController and UsuariosService"

--- src/main/java/com/backend/sigclc/Service/Usuarios/IUsuariosService.java
+++ src/main/java/com/backend/sigclc/Service/Usuarios/IUsuariosService.java
@@ -3,7 +3,6 @@ package com.backend.sigclc.Service.Usuarios;
 import java.util.List;
 
 import org.bson.types.ObjectId;
-
 import com.backend.sigclc.DTO.Usuarios.UsuarioCreateDTO;
 import com.backend.sigclc.DTO.Usuarios.UsuarioResponseDTO;
 import com.backend.sigclc.DTO.Usuarios.UsuarioUpdateDTO;
@@ -12,8 +11,6 @@ public interface IUsuariosService {
     public UsuarioResponseDTO guardarUsuario(UsuarioCreateDTO usuario);
     public List<UsuarioResponseDTO> listarUsuarios();
     public UsuarioResponseDTO  buscarUsuariosPorId(ObjectId id);
-    public UsuarioResponseDTO  buscarPorCorreo(String correoElectronico);
-    public List<UsuarioResponseDTO> buscarPorRol(String rol);
     public UsuarioResponseDTO  actualizarUsuario(ObjectId id, UsuarioUpdateDTO usuario);
     public void sincronizarNombreUsuario(ObjectId id, String nombreCompleto);
     public String eliminarUsuario(ObjectId id);
