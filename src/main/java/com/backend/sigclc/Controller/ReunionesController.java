6995e8a89f76e068c5be32442ea3f6478e04bc40
leiton <sebasleiton05@gmail.com>
2025-11-08 15:54:59 -0500

    ✨feat: Add Update Reunion endpoint in ReunionesController

--- src/main/java/com/backend/sigclc/Controller/ReunionesController.java
+++ src/main/java/com/backend/sigclc/Controller/ReunionesController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.sigclc.DTO.Reuniones.ReunionCreateDTO;
 import com.backend.sigclc.DTO.Reuniones.ReunionResponseDTO;
+import com.backend.sigclc.DTO.Reuniones.ReunionUpdateDTO;
 import com.backend.sigclc.Service.Reuniones.IReunionesService;
 
 @RestController
@@ -34,6 +37,12 @@ public class ReunionesController {
     public ResponseEntity<List<ReunionResponseDTO>> listarReuniones() {
         List<ReunionResponseDTO> response = reunionesService.listarReuniones();
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PatchMapping(value = "/actualizar/{id}", consumes = "multipart/form-data")
+    public ResponseEntity<ReunionResponseDTO> actualizarReunion(@PathVariable org.bson.types.ObjectId id, @ModelAttribute ReunionUpdateDTO dto) {
+        ReunionResponseDTO respuesta = reunionesService.actualizarReunion(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(respuesta);
     }
 
     @DeleteMapping(value = "/eliminar/{id}")
