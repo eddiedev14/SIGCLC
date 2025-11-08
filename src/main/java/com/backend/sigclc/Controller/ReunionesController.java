cd143d9b1ac7458ef4209cae4b259d56279aa4da
leiton <sebasleiton05@gmail.com>
2025-11-08 16:31:59 -0500

    ✨feat: Add endpoints to add and remove ArchivosAdjuntos and LibrosSeleccionados

--- src/main/java/com/backend/sigclc/Controller/ReunionesController.java
+++ src/main/java/com/backend/sigclc/Controller/ReunionesController.java
@@ -2,6 +2,7 @@ package com.backend.sigclc.Controller;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.backend.sigclc.DTO.Reuniones.ReunionCreateDTO;
 import com.backend.sigclc.DTO.Reuniones.ReunionResponseDTO;
@@ -40,13 +43,53 @@ public class ReunionesController {
     }
 
     @PatchMapping(value = "/actualizar/{id}", consumes = "multipart/form-data")
-    public ResponseEntity<ReunionResponseDTO> actualizarReunion(@PathVariable org.bson.types.ObjectId id, @ModelAttribute ReunionUpdateDTO dto) {
+    public ResponseEntity<ReunionResponseDTO> actualizarReunion(@PathVariable ObjectId id, @ModelAttribute ReunionUpdateDTO dto) {
         ReunionResponseDTO respuesta = reunionesService.actualizarReunion(id, dto);
         return ResponseEntity.status(HttpStatus.OK).body(respuesta);
     }
 
+    @PatchMapping("/agregar-libros/{id}")
+    public ResponseEntity<ReunionResponseDTO> agregarLibrosAReunion(@PathVariable ObjectId id, @RequestParam("librosSeleccionadosId") List<String> librosSeleccionadosId) {
+        ReunionResponseDTO response = reunionesService.agregarLibrosAReunion(id, librosSeleccionadosId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping(value = "/agregar-asistentes/{reunionId}", consumes = "multipart/form-data")
+    public ResponseEntity<ReunionResponseDTO> agregarAsistentesAReunion(@PathVariable ObjectId reunionId, @RequestParam("asistenteId") List<String> asistentesIds) {
+        ReunionResponseDTO respuesta = reunionesService.agregarAsistentesAReunion(reunionId, asistentesIds);
+        return ResponseEntity.status(HttpStatus.OK).body(respuesta);
+    }
+
+    @PatchMapping("/eliminar-libros/{id}")
+    public ResponseEntity<ReunionResponseDTO> eliminarLibrosDeReunion(
+            @PathVariable ObjectId id,
+            @RequestParam("librosSeleccionadosId") List<String> librosSeleccionadosId) {
+
+        ReunionResponseDTO response = reunionesService.eliminarLibrosSeleccionadosDeReunion(id, librosSeleccionadosId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping(value = "/eliminar-asistentes/{reunionId}", consumes = "multipart/form-data")
+    public ResponseEntity<ReunionResponseDTO> eliminarAsistentesDeReunion(
+            @PathVariable ObjectId reunionId,
+            @RequestParam("asistenteId") List<String> asistentesIds) {
+
+        ReunionResponseDTO respuesta = reunionesService.eliminarAsistentesDeReunion(reunionId, asistentesIds);
+        return ResponseEntity.status(HttpStatus.OK).body(respuesta);
+    }
+
+    @PatchMapping(value = "/agregar-archivos/{reunionId}", consumes = "multipart/form-data")
+    public ResponseEntity<ReunionResponseDTO> agregarArchivosAReunion(
+            @PathVariable ObjectId reunionId,
+            @RequestParam("archivosAdjuntos") List<MultipartFile> archivosAdjuntos) {
+
+        ReunionResponseDTO respuesta = reunionesService.agregarArchivosAReunion(reunionId, archivosAdjuntos);
+        return ResponseEntity.status(HttpStatus.OK).body(respuesta);
+    }
+
+
     @DeleteMapping(value = "/eliminar/{id}")
-    public ResponseEntity<String> eliminarReunion(@org.springframework.web.bind.annotation.PathVariable org.bson.types.ObjectId id) {
+    public ResponseEntity<String> eliminarReunion(@PathVariable ObjectId id) {
         String respuesta = reunionesService.eliminarReunion(id);
         return ResponseEntity.status(HttpStatus.OK).body(respuesta);
     }
