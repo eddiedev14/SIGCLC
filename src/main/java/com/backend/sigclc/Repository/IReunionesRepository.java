3b4e65cf28a49dedbefe38dd7090bd473314e1cd
leiton <sebasleiton05@gmail.com>
2025-11-11 11:54:13 -0500

    ✨feat: Add synchronization methods to Reuniones in IReunionesRepository

--- src/main/java/com/backend/sigclc/Repository/IReunionesRepository.java
+++ src/main/java/com/backend/sigclc/Repository/IReunionesRepository.java
@@ -1,12 +1,47 @@
 package com.backend.sigclc.Repository;
 
+
+
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
 import com.backend.sigclc.Model.Reuniones.ReunionesModel;
 
 @Repository
 public interface IReunionesRepository extends MongoRepository <ReunionesModel, ObjectId>{
-    
+    //Actualizar nombre de asistente si se modifica
+    @Query("{'asistentes.id': ?0}")
+    @Update("{'$set': {'asistentes.$.nombre': ?1}}")
+    void actualizarNombreAsistente(ObjectId asistenteId, String nuevoNombre);
+
+    // Eliminar asistente de todas las reuniones si se elimina el usuario
+    @Query("{'asistentes.id': ?0}")
+    @Update("{'$pull': {'asistentes': {'id': ?0}}}")
+    void eliminarAsistenteDeReuniones(ObjectId asistenteId);
+
+    // Actualizar título de libro si se modifica
+    @Query("{'librosSeleccionados.id': ?0}")
+    @Update("{'$set': {'librosSeleccionados.$.titulo': ?1}}")
+    void actualizarTituloLibroSeleccionado(ObjectId libroId, String nuevoTitulo);
+
+    // Actualizar autores del libro si se modifican
+    @Query("{'librosSeleccionados.id': ?0}")
+    @Update("{'$set': {'librosSeleccionados.$.autores': ?1}}")
+    void actualizarAutoresLibroSeleccionado(ObjectId libroId, List<String> nuevosAutores);
+
+    // Actualizar géneros del libro si se modifican
+    @Query("{'librosSeleccionados.id': ?0}")
+    @Update("{'$set': {'librosSeleccionados.$.generos': ?1}}")
+    void actualizarGenerosLibroSeleccionado(ObjectId libroId, List<String> nuevosGeneros);
+
+    // Eliminar libro de todas las reuniones si se elimina el libro
+    @Query("{'librosSeleccionados.id': ?0}")
+    @Update("{'$pull': {'librosSeleccionados': {'id': ?0}}}")
+    void eliminarLibroDeReuniones(ObjectId libroId);
+
 }
